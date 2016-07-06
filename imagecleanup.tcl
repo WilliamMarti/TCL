ff5f2739fba01cb6650b15b53c0fb602728f74d1
Ben Marti <william.b.marti@gmail.com>
2016-07-06 10:02:30 -0400

    added imagecleanup file

---
+++ imagecleanup.tcl
@@ -0,0 +1,47 @@
+# Delete all images from flash except for the standard
+# Also remove those images from boot menu
+#
+# William Marti
+#
+#
+
+# Passed in at the cli, image we want to check for
+set standard $argv 0
+
+# grab all image files '.bin'
+set dir [exec {dir flash: | incl .bin}]
+
+array set imagearray {}
+
+set count 0
+set indexcount 0
+
+# loop through every piece of the 'dir flash: | incl .bin'
+# grab the last word of every line, which is the image name
+# hence the '$count % 9', grabs the last word 
+foreach x $dir { 
+
+	if {$count % 9 == 8} { 
+		set imagearray($indexcount) [lindex $dir $count] 
+		incr indexcount
+	} 
+
+	incr count
+}
+
+set count 0
+set size [array size imagearray]
+
+#if the image not the current standard, delete it 
+for {set i 0} {$i < $size} {incr i} {
+
+	set check $imagearray($i)
+	set check [string trim $check]
+
+    if { $check != $standard } {
+    	exec "delete flash:$check"
+    	ios_config "no boot system flash:$check"
+    } 
+}
+
+
